f398bbaef3fca0b338cddcb990c61527128c5433
Mohammed-Ruman <100762375+Mohammed-Ruman@users.noreply.github.com>
2022-03-07 18:41:42 +0530

    Update design-3.java

--- DesignPattern/design-3.java
+++ DesignPattern/design-3.java
@@ -1,41 +1,33 @@
 package com.logic.pack1;
 
 public class E {
-public static void main(String[] args) {
-	
-	for(int i=1; i<5; i++)
-	{
-		for(int j=1; j<=i; j++)
-		{
-			System.out.print("  ");
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("  ");
+			}
+			for (int j = i; j < 5; j++) {
+				System.out.print("* ");
+			}
+			for (int j = i; j <= 5; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
-		for(int j=i; j<5; j++)
-		{
-			System.out.print("* ");
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j < i; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
-		for(int j=i; j<=5; j++)
-		{
-			System.out.print("* ");
-		}
-		System.out.println();	
 	}
-	for(int i=1; i<=5; i++)
-	{
-		for(int j=i; j<=5; j++)
-		{
-			System.out.print("  ");
-		}
-		for(int j=1; j<i; j++)
-		{
-			System.out.print("* ");
-		}
-		for(int j=1; j<=i; j++)
-		{
-			System.out.print("* ");
-		}
-		System.out.println();
-	}
-}
 }
 /*
 Output:
