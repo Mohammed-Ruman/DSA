772f22f1d1d1ac78d29da9e4a561b7f890bc22e7
Mohammed-Ruman <100762375+Mohammed-Ruman@users.noreply.github.com>
2022-03-07 18:40:36 +0530

    Create design-3.java

---
+++ DesignPattern/design-3.java
@@ -0,0 +1,52 @@
+package com.logic.pack1;
+
+public class E {
+public static void main(String[] args) {
+	
+	for(int i=1; i<5; i++)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			System.out.print("  ");
+		}
+		for(int j=i; j<5; j++)
+		{
+			System.out.print("* ");
+		}
+		for(int j=i; j<=5; j++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();	
+	}
+	for(int i=1; i<=5; i++)
+	{
+		for(int j=i; j<=5; j++)
+		{
+			System.out.print("  ");
+		}
+		for(int j=1; j<i; j++)
+		{
+			System.out.print("* ");
+		}
+		for(int j=1; j<=i; j++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+Output:
+  * * * * * * * * *   
+    * * * * * * *    
+      * * * * *    
+        * * *       
+          *             
+        * * *              
+      * * * * *           
+    * * * * * * *       
+  * * * * * * * * *   
+
+*/
